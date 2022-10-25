3b02821c7bab30d71941e84aac269ed03b25d08a
Lewis Powell <57398782+Lewis-Powell@users.noreply.github.com>
2022-10-25 12:42:07 +0100

    Frame Mesh Added
    LP
    
    -Mesh Added
    -BP and C++ class for portal
    -BP for portal manager

---
+++ Source/PortalSystem/Portal.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Portal.h"
+
+// Sets default values
+APortal::APortal()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	//m_RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("m_RootComponent"));
+	//SetRootComponent(m_RootComponent);
+	/*OuterMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Outer"));
+	RootComponent = OuterMesh;*/
+	//OuterMesh->SetupAttachment(m_RootComponent);
+	//OuterMesh->AttachToComponent(m_RootComponent,);
+
+
+	OuterMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = OuterMesh;
+}
+
+// Called when the game starts or when spawned
+void APortal::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APortal::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/PortalSystem/Portal.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Portal.generated.h"
+
+UCLASS()
+class PORTALSYSTEM_API APortal : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APortal();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	//UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	//	UStaticMeshComponent* OuterMesh;
+
+
+private:
+
+	//UProperty Variables
+	UPROPERTY(EditDefaultsOnly, Category = "Physical Components") // meta = (AllowPrivateAccess = "true")
+		UStaticMeshComponent* ProjectileMesh;
+
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* OuterMesh;
+
+	//UPROPERTY(EditAnywhere)
+	//	UStaticMeshComponent* Plane;
+	UPROPERTY(EditAnywhere)
+		USceneComponent* m_RootComponent;
+
+
+};
